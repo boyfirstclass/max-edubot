494c9d46d6527ef665f925a69227f656430a6ff7
boyfirstclass <dolmanovm@gmail.com>
2025-11-15 11:35:58 +0300

    Update Assignment.cs

--- MaxEduBot/Models/Assignment.cs
+++ MaxEduBot/Models/Assignment.cs
@@ -5,7 +5,7 @@ public class Assignment
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid GroupId { get; set; }
     public long CreatedBy { get; set; }
-    public int VariantsCount { get; set; }  // 1..100
+    public int VariantsCount { get; set; } 
     public DateTime DeadlineUtc { get; set; }
     public string Title { get; set; } = "";
     public string? Description { get; set; }
